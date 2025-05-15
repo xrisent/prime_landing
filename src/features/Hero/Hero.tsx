addfeab9bf51d960b52a00969233d485122178c5
xrisent <ormonov31261@gmail.com>
2025-05-15 17:47:35 +0600

    finished hero without adaptive

--- src/features/Hero/Hero.tsx
+++ src/features/Hero/Hero.tsx
@@ -12,29 +12,31 @@ export const Hero: React.FC = () => {
   return (
     <section className={styles.hero}>
       <div className={`container ${styles.containerHero}`}>
-        <div className={styles.heroLeft}>
-          <HeroText h1Text={t("h1")} pText={t("p")} />
-          <div className={styles.buttons}>
-            <TelegramButton size="medium" text={t("telegram")} />
-            <WhatsappButton size="medium" text={t("whatsapp")} />
+        <div className={styles.hero__box}>
+          <div className={styles.heroLeft}>
+            <HeroText h1Text={t("h1")} pText={t("p")} />
+            <div className={styles.buttons}>
+              <TelegramButton size="medium" text={t("telegram")} />
+              <WhatsappButton size="medium" text={t("whatsapp")} />
+            </div>
+            <Achievements
+              li1First={t("li1First")}
+              li1Second={t("li1Second")}
+              li2First={t("li2First")}
+              li2Second={t("li2Second")}
+              li3First={t("li3First")}
+              li3Second={t("li3Second")}
+            />
           </div>
-          <Achievements
-            li1First={t("li1First")}
-            li1Second={t("li1Second")}
-            li2First={t("li2First")}
-            li2Second={t("li2Second")}
-            li3First={t("li3First")}
-            li3Second={t("li3Second")}
-          />
-        </div>
-        <div className={styles.heroRight}>
-          <FormHero
-            pText={t("formDescription")}
-            name=""
-            email=""
-            number=""
-            message=""
-          />
+          <div className={styles.heroRight}>
+            <FormHero
+              pText={t("formDescription")}
+              name=""
+              email=""
+              number=""
+              message=""
+            />
+          </div>
         </div>
       </div>
     </section>
--- src/widgets/formHero/FormHero.tsx
+++ src/widgets/formHero/FormHero.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 
-import { useState } from 'react';
-import PhoneInput from 'react-phone-number-input';
-import 'react-phone-number-input/style.css';
-import styles from './FormHero.module.scss';
-import { FormHeroProps } from './types/types';
-import { useTranslations } from 'next-intl';
+import { useState } from "react";
+import PhoneInput from "react-phone-number-input";
+import "react-phone-number-input/style.css";
+import "./FormHero.scss";
+import { FormHeroProps } from "./types/types";
+import { useTranslations } from "next-intl";
 
 export const FormHero: React.FC<FormHeroProps> = ({
   pText,
@@ -14,65 +14,65 @@ export const FormHero: React.FC<FormHeroProps> = ({
   number: initialNumber,
   message: initialMessage,
 }) => {
-    const t = useTranslations('FormHero')
+  const t = useTranslations("FormHero");
   const [name, setName] = useState(initialName);
   const [email, setEmail] = useState(initialEmail);
   const [phone, setPhone] = useState<string | undefined>(initialNumber);
   const [message, setMessage] = useState(initialMessage);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
   };
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <p className={styles.description}>{pText}</p>
-      
-      <div className={styles.formGroup}>
+    <form className='formHero' onSubmit={handleSubmit}>
+      <p className='formHero__description'>{pText}</p>
+
+      <div className='formHero__formGroup'>
         <input
           type="text"
           id="name"
-          placeholder={t('name')}
+          placeholder={t("name")}
           value={name}
           onChange={(e) => setName(e.target.value)}
           required
         />
       </div>
-      
-      <div className={styles.formGroup}>
+
+      <div className='formHero__formGroup'>
         <input
           type="email"
           id="email"
-          placeholder={t('email')}
+          placeholder={t("email")}
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
         />
       </div>
-      
-      <div className={styles.formGroup}>
+
+      <div className='formHero__formGroup'>
         <PhoneInput
           international
-        //   defaultCountry="KG"
+          //   defaultCountry="KG"
           value={phone}
           onChange={setPhone}
-          placeholder={t('number')}
+          placeholder={t("number")}
         />
       </div>
-      
-      <div className={styles.formGroup}>
+
+      <div className='formHero__formGroup'>
         <textarea
           id="message"
           value={message}
-          placeholder={t('message')}
+          placeholder={t("message")}
           onChange={(e) => setMessage(e.target.value)}
           rows={4}
         />
       </div>
-      
-      <button type="submit" className={styles.submitButton}>
-        {t('send')}
+
+      <button type="submit" className='formHero__button'>
+        {t("send")}
       </button>
     </form>
   );
