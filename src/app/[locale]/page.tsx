e3d5fb96e88976b302314eca94aa4f0309b3f9c5
xrisent <ormonov31261@gmail.com>
2025-05-16 13:31:49 +0600

    footer and map

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,12 +3,14 @@ import styles from "./page.module.scss";
 import { useTranslations } from "next-intl";
 import { WorkUs } from "@/features/WorkUs/WorkUs";
 import { Hero } from "@/features/Hero/Hero";
+import { Map } from "@/features/Map/Map";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-        <Hero/>
-      <WorkUs/>
+      <Hero />
+      <WorkUs />
+      <Map />
     </main>
   );
 }
--- src/widgets/footer/Footer.tsx
+++ src/widgets/footer/Footer.tsx
@@ -1,14 +1,13 @@
 import styles from './Footer.module.scss'
 import mobile from './FooterMobile.module.scss'
-import Image from 'next/image'
-// import { useRouter } from 'next/navigation'
 
 export const Footer: React.FC = () => {
     return (
         <footer className={`${styles.footer} ${mobile.footer}`}>
-            <div className="container">
+            <div className={`container ${styles.containerFooter}`}>
                 <div className={`${styles.footer__box} ${mobile.footer__box}`}>
-                    
+                    <div className={styles.line}></div>
+                    <p>2025 PRIME SETUP. All rights reserved</p>
                 </div>
             </div>
         </footer>
